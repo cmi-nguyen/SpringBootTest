001132344bed1034a3c85389dc2945903098238f
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2022-12-22 00:04:57 +0700

    CRUD implementation

--- src/main/java/com/example/demo/services/CustomerService.java
+++ src/main/java/com/example/demo/services/CustomerService.java
@@ -5,5 +5,18 @@ import com.example.demo.entities.Customer;
 import java.util.List;
 
 public interface CustomerService {
+    // Save Operation
+    Customer saveCustomer(Customer customer);
+
+    // Read Operation
     List<Customer> fetchCustomerList();
+
+    // Update Operation
+    Customer updateCustomer(Customer customer,int id_KH);
+
+    // Delete Operation
+
+    void deleteCustomerByID(int id_KH);
+
+
 }
--- src/main/java/com/example/demo/services/CustomerServiceImpl.java
+++ src/main/java/com/example/demo/services/CustomerServiceImpl.java
@@ -6,14 +6,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
     @Autowired
     private CustomerRepository customerRepository;
 
+    // Read operation implementation
     @Override
     public List<Customer> fetchCustomerList() {
         return customerRepository.findAll();
     }
+    // Update
+    @Override
+    public Customer updateCustomer(Customer customer, int id_KH) {
+        Customer cusDB = customerRepository.findById(id_KH).get();
+        if (Objects.nonNull(customer.getCustomer_id())){
+            cusDB.setCustomer_id(customer.getCustomer_id());
+        }
+        if  (Objects.nonNull(customer.getFirst_name()) && !"".equalsIgnoreCase(customer.getFirst_name())){
+            cusDB.setFirst_name(customer.getFirst_name());
+        }
+        if (Objects.nonNull(customer.getLast_name()) && !"".equalsIgnoreCase(customer.getLast_name())){
+            cusDB.setLast_name(customer.getLast_name());
+        }
+        if (Objects.nonNull(customer.getEmail()) && !"".equalsIgnoreCase(customer.getEmail())){
+            cusDB.setEmail(customer.getEmail());
+        }
+        if (Objects.nonNull(customer.getPhone())){
+            cusDB.setPhone(customer.getPhone());
+        }
+        if (Objects.nonNull(customer.getStatus())){
+            cusDB.setStatus(customer.getStatus());
+        }
+        return saveCustomer(cusDB);
+    }
+
+    // Save operation
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+    // Delete Operation
+    @Override
+    public void deleteCustomerByID(int id_KH) {
+        customerRepository.deleteById(id_KH);
+    }
+
 }
